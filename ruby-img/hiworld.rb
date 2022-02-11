8311fd1bcaa256b4c5be20810c7bd48f4cb14787
David <86898130+narodn1k@users.noreply.github.com>
2022-02-11 13:59:23 -0500

    Comment out gems for later

--- ruby-img/hiworld.rb
+++ ruby-img/hiworld.rb
@@ -1,4 +1,10 @@
 #!/usr/bin/env ruby
 
+# require 'git'
+
 puts "Hello world."
 puts "This should do something later."
+
+# Git.clone('git://github.com/narodn1k/neon-buildbox.git', 'clone.git' )
+
+# puts "Got the stuff"
